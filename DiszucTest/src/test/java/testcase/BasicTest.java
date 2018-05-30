d983283a62eeb14ebed65be8725fb07837abeb3c
oleanderr <39108520+oleanderr@users.noreply.github.com>
2018-05-30 19:32:24 +0800

    added

--- DiszucTest/src/test/java/testcase/BasicTest.java
+++ DiszucTest/src/test/java/testcase/BasicTest.java
@@ -33,8 +33,8 @@ public class BasicTest {
 
     @AfterMethod
     public void teardown(){
-//        if(driver!=null){
-//            driver.quit();
-//        }
+        if(driver!=null){
+            driver.quit();
+        }
     }
 }
